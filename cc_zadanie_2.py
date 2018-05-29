443d28e7b5724ec4ebe5f26b2bf26cb508f29bd6
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2018-05-29 09:26:09 +0200

    zadanie 2

--- cc_zadanie_2.py
+++ cc_zadanie_2.py
@@ -1,9 +1,18 @@
 # opis
-# Wyswietlenie napisu "Hello world!" na czacie
+# Uzycie funkcji `mc.setBlock(x, y, z, material)` tak, 
+# zeby rozstawic 5 roznych materialow w pozycjach:
+# 1) 0, 0, 0 z materialem block.GLASS
+# 2) 0, 10, 0 z materialem block.WOOD
+# 3) 0, 0, 10 z materialem  block.MELON
+# 4) 0, 10, 10 z materialem block.GOLD_BLOCK
+# 5) W dowolnym miejscu, z dowolnym materialem. Znajdz i zaprezentuj
 
 
 from mine import *
 
 mc = Minecraft()
 
-mc.postToChat("Hello world!")
+mc.setBlock(0, 0, 0, block.GLASS)
+mc.setBlock(0, 10, 0, block.WOOD)
+mc.setBlock(0, 0, 10, block.MELON)
+mc.setBlock(10, 10, 10, block.GOLD_BLOCK)
