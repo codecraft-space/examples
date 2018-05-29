4b93cb39697adcd0aed093a1eda31985e37f1d4b
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2018-05-29 10:14:19 +0200

    zadanie 6

---
+++ cc_zadanie_6.py
@@ -0,0 +1,12 @@
+# opis
+# Zmieniajac 2 (wysokosc) parametr polecenia 'setBlock', zbuduj piaskowa wieze (RED_SAND)
+# Startujac z pozycji 0 0 0. 
+# Wieza powinna miec wysokosc 500 elementow
+
+from mine import *
+
+mc = Minecraft()
+
+
+for wysokosc in range(500):
+    mc.setBlock(0, wysokosc, 0, block.RED_SAND)
