d5598d988d3915213d9dca07ab5f97e3a2b0240b
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2018-05-29 10:14:16 +0200

    zadanie 5

---
+++ cc_zadanie_5.py
@@ -0,0 +1,10 @@
+# opis
+# Na czacie Minecraft wyswietl liczby od 0 do 5. Sprawdz zadanie pierwsze aby zobaczyc jak wypisywac na czacie.
+
+from mine import *
+
+mc = Minecraft()
+
+
+for numer in range(5):
+    mc.postToChat(str(numer))
